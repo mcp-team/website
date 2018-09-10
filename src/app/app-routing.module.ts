8099c7e55a9957c46e7a8bee4bc6a12f304a6927
Daniel Wolff <wolffd@mcmaster.ca>
2018-09-10 12:42:12 -0400

    merging about-us component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SponsorsViewComponent } from './components/sponsors-view/sponsors-view.component';
+import { AboutUsViewComponent } from './components/about-us-view/about-us-view.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'sponsors', pathMatch: 'full' },
+  { path: '', redirectTo: 'about-us', pathMatch: 'full' },
+  { path: 'home', redirectTo: 'about-us', pathMatch: 'full' }, // temporary?
+  { path: 'about-us', component: AboutUsViewComponent },
   { path: 'sponsors', component: SponsorsViewComponent }
 ];
 
