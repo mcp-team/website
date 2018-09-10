6c1e21f3011f1a011e723244489b4140b05c061a
Daniel Wolff <wolffd@mcmaster.ca>
2018-09-10 15:13:17 -0400

    added content to deadend-view component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SponsorsViewComponent } from './components/sponsors-view/sponsors-view.component';
 import { AboutUsViewComponent } from './components/about-us-view/about-us-view.component';
+import { DeadendViewComponent } from './components/deadend-view/deadend-view.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'about-us', pathMatch: 'full' },
   { path: 'home', redirectTo: 'about-us', pathMatch: 'full' }, // temporary?
   { path: 'about-us', component: AboutUsViewComponent },
-  { path: 'sponsors', component: SponsorsViewComponent }
+  { path: 'sponsors', component: SponsorsViewComponent },
+  { path: 'contact-us', component: DeadendViewComponent },
+  { path: 'members', component: DeadendViewComponent },
 ];
 
 @NgModule({
